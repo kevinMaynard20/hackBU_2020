7f24b62bf5cf076b0345bb0950f574197fb8717a
Kevin Maynard <kevinmaynard@Kevins-MacBook-Pro.local>
2021-02-06 17:35:07 -0500

    Try this anyone

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,38 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-// ignore_for_file: public_member_api_docs
-
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter_example/lite_mode.dart';
-import 'animate_camera.dart';
-import 'map_click.dart';
-import 'map_coordinates.dart';
-import 'map_ui.dart';
-import 'marker_icons.dart';
-import 'move_camera.dart';
-import 'padding.dart';
-import 'page.dart';
-import 'place_circle.dart';
-import 'place_marker.dart';
-import 'place_polygon.dart';
-import 'place_polyline.dart';
-import 'scrolling_map.dart';
-import 'snapshot.dart';
-import 'tile_overlay.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-final List<GoogleMapExampleAppPage> _allPages = <GoogleMapExampleAppPage>[
-  MapUiPage(),
-  MapCoordinatesPage(),
-  MapClickPage(),
-  AnimateCameraPage(),
-  MoveCameraPage(),
-  PlaceMarkerPage(),
-  MarkerIconsPage(),
-  ScrollingMapPage(),
-  PlacePolylinePage(),
-  PlacePolygonPage(),
-  PlaceCirclePage(),
-  PaddingPage(),
-  SnapshotPage(),
-  LiteModePage(),
-  TileOverlayPage(),
-];
+void main() => runApp(MyApp());
 
-class MapsDemo extends StatelessWidget {
-  void _pushPage(BuildContext context, GoogleMapExampleAppPage page) {
-    Navigator.of(context).push(MaterialPageRoute<void>(
-        builder: (_) => Scaffold(
-          appBar: AppBar(title: Text(page.title)),
-          body: page,
-        )));
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('GoogleMaps examples')),
-      body: ListView.builder(
-        itemCount: _allPages.length,
-        itemBuilder: (_, int index) => ListTile(
-          leading: _allPages[index].leading,
-          title: Text(_allPages[index].title),
-          onTap: () => _pushPage(context, _allPages[index]),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Date Night'),
+          backgroundColor: Colors.red[700],
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
         ),
       ),
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(home: MapsDemo()));
 }
